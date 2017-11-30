b64def70a5b60707846f53f56df59c6e97199d4b
andrew-semenov <andrew.semenov.sas@gmail.com>
2017-11-30 22:21:00 +0200

    Update Category.java

--- app/src/main/java/com/easychoice/easy_choice/Category.java
+++ app/src/main/java/com/easychoice/easy_choice/Category.java
@@ -9,5 +9,7 @@ public class Category extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        
+        Button btn = FindViewById(R.layout.btnAdd);
     }
 }
